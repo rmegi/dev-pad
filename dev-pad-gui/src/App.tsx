186c3583e7bd53c6648f72acf23f7136871d9cde
Rmegi <roy.megidish@gmail.com>
2026-02-06 14:47:31 +0200

    feat: add URL state synchronization with compression and update dependencies

--- dev-pad-gui/src/App.tsx
+++ dev-pad-gui/src/App.tsx
@@ -1,13 +1,27 @@
 import { Route, Routes } from "react-router";
+import { useEffect } from "react";
+import { useSetAtom } from "jotai";
+
 import HomePage from "./pages/HomePage";
+import { tabsAtom, activeTabIdAtom } from "./atoms/tabsAtom";
+import { readStateFromUrl } from "./utils/urlState";
 
 function App() {
+  const setTabs = useSetAtom(tabsAtom);
+  const setActiveTabId = useSetAtom(activeTabIdAtom);
+
+  useEffect(() => {
+    const urlState = readStateFromUrl();
+    if (!urlState) return;
+
+    setTabs(urlState.tabs);
+    setActiveTabId(urlState.activeTabId);
+  }, [setTabs, setActiveTabId]);
+
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   );
 }
 
---
+++ dev-pad-gui/src/atoms/syncUrlAtom.ts
@@ -0,0 +1,17 @@
+import { atom } from "jotai";
+import { tabsAtom, activeTabIdAtom } from "./tabsAtom";
+import { writeStateToUrl } from "../utils/urlState";
+
+let timer: number | null = null;
+
+export const syncUrlAtom = atom(null, (get) => {
+  const tabs = get(tabsAtom);
+  const activeTabId = get(activeTabIdAtom);
+
+  if (timer) clearTimeout(timer);
+  console.log("SYNC URL ATOM RUNNING", { tabs, activeTabId });
+
+  timer = window.setTimeout(() => {
+    writeStateToUrl(tabs, activeTabId);
+  }, 400);
+});
